42750c8b578adb2c2cc4d5d822f9fc0dfb586da8
Abhishekkumar071 <abhishekkumarbgp0917@gmail.com>
2026-02-10 22:58:16 +0530

    Time: 50 ms (98.90%) | Memory: 46.9 MB (84.49%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -1,18 +1,20 @@
 class Solution {
+    short[] freq = new short[100001];
     public int longestBalanced(int[] nums) {
         int n = nums.length;
         int ans=0;
         
         for(int i=0; i<n; i++){
-            Set<Integer> even = new HashSet<>();
-            Set<Integer> odd = new HashSet<>();
+            int[] count = new int[2];
             for(int j=i; j<n; j++){
-                if(nums[j]%2==0) even.add(nums[j]);
-                else odd.add(nums[j]);
-                if(odd.size()==even.size()){
+                if(++freq[nums[j]]==1){
+                    count[nums[j]&1]++;
+                }
+                if(count[0]==count[1]){
                     ans=Math.max(ans, j-i+1);
                 }
             }
+            for(int j=i; j<n; j++) freq[nums[j]]=0;
         }
         return ans;
     }
