d2ec090a3b3a5ef0ba3b704d894352f9ba655ad9
Abhishekkumar071 <abhishekkumarbgp0917@gmail.com>
2026-03-10 01:54:02 +0530

    Time: 223 ms (15.89%) | Memory: 53.9 MB (48.64%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -1,34 +1,36 @@
 class Solution {
 
-        private int dfs(int i, int prev, int[][] grid, Integer[][] dp){
-                if(i == grid.length) return 0;
+    private int dfs(int i, int prev, int[][] grid, Integer[][] dp) {
+        if (i == grid.length)
+            return 0;
 
-                        if(dp[i][prev] != null) return dp[i][prev];
+        if (dp[i][prev] != null)
+            return dp[i][prev];
 
-                                int min = Integer.MAX_VALUE;
-                                        int n = grid.length;
+        int min = Integer.MAX_VALUE;
+        int n = grid.length;
 
-                                                for(int c = 0; c < n; c++){
-                                                            if(c != prev){
-                                                                            int curr = grid[i][c] + dfs(i + 1, c, grid, dp);
-                                                                                            min = Math.min(min, curr);
-                                                                                                        }
-                                                                                                                }
+        for (int c = 0; c < n; c++) {
+            if (c != prev) {
+                int curr = grid[i][c] + dfs(i + 1, c, grid, dp);
+                min = Math.min(min, curr);
+            }
+        }
 
-                                                                                                                        return dp[i][prev] = min;
-                                                                                                                            }
+        return dp[i][prev] = min;
+    }
 
-                                                                                                                                public int minFallingPathSum(int[][] grid) {
+    public int minFallingPathSum(int[][] grid) {
 
-                                                                                                                                        int n = grid.length;
-                                                                                                                                                int ans = Integer.MAX_VALUE;
+        int n = grid.length;
+        int ans = Integer.MAX_VALUE;
 
-                                                                                                                                                        Integer[][] dp = new Integer[n][n];
+        Integer[][] dp = new Integer[n][n];
 
-                                                                                                                                                                for(int c = 0; c < n; c++){
-                                                                                                                                                                            ans = Math.min(ans, grid[0][c] + dfs(1, c, grid, dp));
-                                                                                                                                                                                    }
+        for (int c = 0; c < n; c++) {
+            ans = Math.min(ans, grid[0][c] + dfs(1, c, grid, dp));
+        }
 
-                                                                                                                                                                                            return ans;
-                                                                                                                                                                                                }
-                                                                                                                                                                                                }
+        return ans;
+    }
+}
