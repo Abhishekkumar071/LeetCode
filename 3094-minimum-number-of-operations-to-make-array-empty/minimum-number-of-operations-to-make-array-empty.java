10b8fe784cb2897963b07b2b6d4582151f1093a0
Abhishekkumar071 <abhishekkumarbgp0917@gmail.com>
2026-03-17 00:17:34 +0530

    Time: 100 ms (5.09%) | Memory: 91 MB (5.99%) - LeetSync

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int ele: nums){
+            map.put(ele, map.getOrDefault(ele, 0)+1);
+        }
+        int ans = 0;
+        for(int val: map.values()){
+            if(val==1) return -1;
+            if(val%3==0) ans += val/3;
+            else{
+                ans += val/3;
+                val = (val+1/3);
+                System.out.println(val);
+                if(val>0)ans +=1;
+            }
+        }
+        return ans;
+    }
+}
