4496f960093f567974d5bad5cd25b7ec2489cfa7
Abhishekkumar071 <abhishekkumarbgp0917@gmail.com>
2026-04-04 23:23:13 +0530

    Time: 3 ms (77.60%) | Memory: 43.2 MB (34.21%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,31 +1,45 @@
-import java.util.*;
-
 class Solution {
+    public class Pair{
+        int val;
+        char ch;
+        Pair(int val, char ch){
+            this.val = val;
+            this.ch = ch;
+        }
+    }
     public String reorganizeString(String s) {
         int[] freq = new int[26];
-        int n = s.length();
-
-        for(char ch: s.toCharArray()) freq[ch-'a']++;
-
-        for(int f: freq){
-            if(f > (n+1)/2) return "";
+        for(char ch: s.toCharArray()){
+            freq[ch-'a']++;
         }
-
-       
+        int n=s.length();
+        for(int i=0; i<26; i++){
+            if(freq[i]>(n+1)/2) return "";
+        }
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->b.val-a.val);
         StringBuilder sb = new StringBuilder();
-        int last = -1;
-        for(int i=0; i<n; i++){
-            int pick = -1;
-            for(int j=0; j<26; j++){
-                if(freq[j] > 0 && j!=last){
-                   if(pick==-1 || freq[j]>freq[pick]) pick=j;
-                }
+
+        for(int i=0; i<26; i++){
+            if(freq[i]>0){
+                pq.add(new Pair(freq[i], (char)('a'+i)));
             }
-            sb.append((char)('a'+pick)); 
-            freq[pick]--;
-            last=pick;
         }
 
+        while(!pq.isEmpty()){
+            Pair f = pq.poll();
+            if(sb.length()>0 && sb.charAt(sb.length()-1)==f.ch){
+                Pair s1 = pq.poll();
+                sb.append(s1.ch);
+                s1.val--;
+                if(s1.val>0) pq.add(s1);
+                pq.add(f);
+            }
+            else{
+                sb.append(f.ch);
+                f.val--;
+                if(f.val>0) pq.add(f);
+            }
+        }
         return sb.toString();
     }
 }
