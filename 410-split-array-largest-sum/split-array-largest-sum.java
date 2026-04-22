98916d1b89ddae4da3c9706c87c9d7756fde94ea
Abhishekkumar071 <abhishekkumarbgp0917@gmail.com>
2026-04-22 21:59:30 +0530

    Time: 4 ms (6.39%) | Memory: 43.3 MB (8.97%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,38 @@
+class Solution {
+  
+
+    private boolean cheack(int[] arr, int k, int i1){
+        int s=1;
+        int sum=0;
+        for(int i=0; i<arr.length; i++){
+            if(sum+arr[i]<=i1){
+                sum+= arr[i];
+            }
+            else{
+                s++;
+                sum=arr[i];
+            }
+        }
+        return (s<=k);
+    }
+    public int splitArray(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        if(n<k) return -1;
+        
+        int min = Arrays.stream(arr).max().getAsInt();
+        int max = Arrays.stream(arr).sum();
+        
+        int res = -1;
+        
+        while(min<=max){
+            int mid = ((max+min)/2);
+            if(cheack(arr, k, mid)){
+                res=mid;
+                max = mid-1;
+            }
+            else min=mid+1;
+        }
+        return res;
+    }
+}
