b341da1e30289aa7da87f3719821ada27438e00c
Abhishekkumar071 <abhishekkumarbgp0917@gmail.com>
2026-05-05 01:40:04 +0530

    Time: 3 ms (9.22%) | Memory: 46.2 MB (94.17%) - LeetSync

---
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.java
@@ -0,0 +1,9 @@
+class Solution {
+    public long countSubstrings(String s, char c) {
+        long ans = 0;
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            if(s.charAt(i)==c)ans++;
+        }return (ans*(ans+1))/2;
+    }
+}
