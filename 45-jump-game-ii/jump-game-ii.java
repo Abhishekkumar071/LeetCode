33b22cdd45de3d77560a071bbd07c719a8ed668d
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-05-17 22:10:57 +0530

    Time: 1 ms (99.59%) | Memory: 47.4 MB (20.46%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,20 +1,18 @@
 class Solution {
-    int[] memo;
     public int jump(int[] nums) {
-        memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        return solve(nums, 0);
-    }
-    private int solve(int[] nums, int idx){
-        if(idx==nums.length-1) return 0;
-        if(idx>nums.length-1) return 1000000007;
-
-        if(memo[idx]!=-1) return memo[idx];
-        int maxJump = nums[idx];
-        int min=1000000007;
-        for(int step=1; step<=maxJump; step++){
-            min = Math.min(min, 1+solve(nums, idx+step));
+        int near = 0;
+        int far = 0;
+        int jump=0;
+        
+        while(far<nums.length-1){
+            int farthest = 0;
+            for(int i=near; i<=far; i++){
+                farthest = Math.max(farthest, i+nums[i]);
+            }
+            near = far+1;
+            far = farthest;
+            jump++;
         }
-        return memo[idx]= min;
+        return jump;
     }
 }
