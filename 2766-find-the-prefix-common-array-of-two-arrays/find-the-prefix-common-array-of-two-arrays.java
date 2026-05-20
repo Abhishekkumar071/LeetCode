b68c7d0e2184febf077602e20f121959f577d723
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-05-21 01:11:01 +0530

    Time: 1 ms (100.00%) | Memory: 47.4 MB (51.66%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -3,14 +3,12 @@ class Solution {
        int n = A.length;
        int[] ans = new int[n];
        int count=0;
-       Set<Integer> set1 = new HashSet<>();
-       Set<Integer> set2 = new HashSet<>();
+       boolean[] seen = new boolean[n];
        for(int i=0; i<n; i++){
-           
-           set1.add(A[i]);
-           if(set2.contains(A[i])) count++;
-           set2.add(B[i]);
-           if(set1.contains(B[i])) count++; 
+           if(seen[A[i]-1]) count++;
+           else seen[A[i]-1]=true;
+           if(seen[B[i]-1]) count++;
+           else seen[B[i]-1]=true;
            ans[i]=count;         
        }
        return ans;
