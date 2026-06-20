5be8a57be302dffd2cac8dabada4d0ca9e777080
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-06-20 12:32:47 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (6.90%) - LeetSync

---
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -0,0 +1,45 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray arr) {
+        int n = arr.length();
+        int l = 0;
+        int r = n-1;
+        while(l!=r){
+            int mid = l+(r-l)/2;
+            if(arr.get(mid)<arr.get(mid+1)) l=mid+1;
+            else r=mid;
+        }
+        // System.out.println(l+" "+r);
+        int top=r;
+        l=0;
+        while(l!=r){
+            int mid = l+(r-l)/2;
+            int a = arr.get(mid);
+            // System.out.print(mid);
+            if(a==target) return mid;
+            else if(a<target) l=mid+1;
+            else if(a>target) r=mid;
+        }
+        if(arr.get(l) == target) return l;
+        l=top+1; r=n-1;
+        while(l != r){
+            int mid = l + (r - l) / 2;
+            int a = arr.get(mid);
+        
+            if(a == target) return mid;
+            else if(a > target) l = mid + 1; // descending
+            else r = mid;                    // descending
+        }
+
+if(l < n && arr.get(l) == target) return l;
+        return -1;
+    }
+}
