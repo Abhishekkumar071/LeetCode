1fc68c8dcf2c99a5e2924cd17003d767d8307e31
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-06-22 02:08:47 +0530

    Time: 6 ms (89.46%) | Memory: 83 MB (83.91%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -1,11 +1,25 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
-        int ans=0;
-        for(int ele: costs){
-            if(coins<ele) break;
-            ans++;
-            coins-=ele;
-        }return ans;
+        int max = 0;
+        for (int x : costs) {
+            max = Math.max(max, x);
+        }
+        int[] freq = new int[max + 1];
+
+        for (int c : costs) {
+            freq[c]++;
+        }
+
+        int ans = 0;
+
+        for (int cost = 1; cost <= max; cost++) {
+            while (freq[cost] > 0 && coins >= cost) {
+                coins -= cost;
+                freq[cost]--;
+                ans++;
+            }
+        }
+
+        return ans;
     }
 }
