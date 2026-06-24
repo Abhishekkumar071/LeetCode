17546f4dc5cc6bd037f39fb5bad7f608cfd90695
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-06-24 17:12:07 +0530

    Time: 19 ms (25.69%) | Memory: 45.1 MB (66.68%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int countAll(TreeNode root, long target) {
+        if (root == null) return 0;
+
+        int count = 0;
+
+        if (root.val == target) {
+            count++;
+        }
+
+        count += countAll(root.left, target - root.val);
+        count += countAll(root.right, target - root.val);
+
+        return count;
+    }
+
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) return 0;
+
+        return countAll(root, targetSum)
+             + pathSum(root.left, targetSum)
+             + pathSum(root.right, targetSum);
+    }
+}
