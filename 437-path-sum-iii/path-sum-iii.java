72a303bac8b289e4b85e573b950616a4d0f40a56
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-06-24 17:50:48 +0530

    Time: 3 ms (98.11%) | Memory: 46.4 MB (36.96%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.java
+++ 437-path-sum-iii/path-sum-iii.java
@@ -14,22 +14,24 @@
  * }
  */
 class Solution {
-    int countAll(TreeNode root, int t, HashMap<Long, Integer> map, long sum){
+    long sum=0l;
+    int countAll(TreeNode root, int t, HashMap<Long, Integer> map){
         if(root==null) return 0;
         sum+=root.val;
         int count=map.getOrDefault(sum-t, 0);
         map.put(sum, map.getOrDefault(sum, 0)+1);
       
-        count+=countAll(root.left, t,map,  sum);
-        count+=countAll(root.right, t, map, sum);
+        count+=countAll(root.left, t,map);
+        count+=countAll(root.right, t, map);
 
-         map.put(sum, map.get(sum)-1);
+        map.put(sum, map.get(sum)-1);
         if(map.get(sum)==0) map.remove(sum);
+        sum-=root.val;
         return count;
     }
     public int pathSum(TreeNode root, int t) {
         HashMap<Long, Integer> map = new HashMap<>();
         map.put(0L, 1);
-        return countAll(root, t, map, 0);
+        return countAll(root, t, map);
     }
 }
