94583d968ecd016d160e24eebe28c0e26f8d3657
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-07-08 08:58:55 +0530

    Time: 391 ms (98.49%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -2,3 +2,14 @@ SELECT DISTINCT author_id as id
 FROM Views
 WHERE author_id = viewer_id
 ORDER BY id;
+
+-- SQL upar se neeche nahi chalti. Ye logically is order me kaam karti hai:
+
+-- 1. FROM
+-- 2. WHERE
+-- 3. GROUP BY
+-- 4. HAVING
+-- 5. SELECT
+-- 6. DISTINCT
+-- 7. ORDER BY
+-- 8. LIMIT
