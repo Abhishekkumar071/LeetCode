7a395ba1333b055a22e578fa75f443077cc4f86f
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-07-22 22:56:35 +0530

    Time: 3 ms (54.82%) | Memory: 48.7 MB (33.49%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,24 +1,24 @@
 class Solution {
-    List<String> ans;
-    void solve(int i, String s, String curr, List<String> w){
-        if(i>=s.length()) {
-            ans.add(curr);
-            return;
+    List<String> solve(String s, List<String> w1){
+        if (s.equals("")) {
+            List<String> base = new ArrayList<>();
+            base.add("");
+            return base;
         }
-        for(int j=i; j<s.length(); j++){
-            String word = s.substring(i, j+1);
-            if(w.contains(word)){
-                String temp = curr;
-                if(curr.length()>0) curr+=" ";
-                curr+=word;
-                solve(j+1, s, curr, w);
-                curr=temp;
+        List<String> ans = new ArrayList<>();
+        for(int l=1; l<=s.length(); l++){
+            String curr = s.substring(0, l);
+            if(w1.contains(curr)){
+                List<String> remain = solve(s.substring(l), w1);
+                for(String w: remain){
+                    if(w.equals("")) ans.add(curr);
+                    else ans.add(curr+" "+w);
+                }
             }
         }
+        return ans;
     }
     public List<String> wordBreak(String s, List<String> wordDict) {
-        ans = new ArrayList<>();
-        solve(0, s,"", wordDict);
-        return ans;
+        return solve(s, wordDict);
     }
 }
