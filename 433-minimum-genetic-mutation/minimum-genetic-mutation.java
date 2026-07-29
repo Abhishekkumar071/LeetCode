4af9da1452bdd0f97b2d221ef786d34ac28c0544
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-07-29 10:50:46 +0530

    Time: 1 ms (93.40%) | Memory: 42.8 MB (44.50%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.java
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.java
@@ -1,37 +1,33 @@
 class Solution {
-    public class Pair{
-        String s;
-        int l;
-        Pair(String s, int l){
-            this.s=s;
-            this.l=l;
-        }
-    }
     char[] arr = {'A', 'C', 'G', 'T'};
     int bfs(String s, String e, HashSet<String> set){
-        Queue<Pair>  q = new LinkedList<>();
-        q.offer(new Pair(s, 0));
+        Queue<String>  q = new LinkedList<>();
+        q.offer(s);
+        int level=0;
         while(!q.isEmpty()){
-            Pair curr = q.poll();
-            String newS = curr.s;
-            int level = curr.l;
-            if(newS.equals(e)) return level;
-            char[] sArr = newS.toCharArray();
-            for(int i=0; i<sArr.length; i++){
-                char prev = sArr[i];
-                for(char ch: arr){
-                    if(ch==prev) continue;
-                    sArr[i]=ch;
-                    String nn = new String(sArr);
-                    // System.out.println(nn);
-                    if(set.contains(nn)){
-                        System.out.println(nn);
-                        q.offer(new Pair(nn, level+1));
-                        set.remove(nn);
+            int size = q.size();
+            for(int ir=0; ir<size; ir++){
+                String curr = q.poll();
+                if(curr.equals(e)) return level;
+                char[] sArr = curr.toCharArray();
+                for(int i=0; i<sArr.length; i++){
+                    char prev = sArr[i];
+                    for(char ch: arr){
+                        if(ch==prev) continue;
+                        sArr[i]=ch;
+                        String nn = new String(sArr);
+                        // System.out.println(nn);
+                        if(set.contains(nn)){
+                            System.out.println(nn);
+                            q.offer(nn);
+                            set.remove(nn);
+                        }
+                        sArr[i]=prev;
                     }
-                    sArr[i]=prev;
                 }
             }
+            level++;
+            
         }
         return -1;
     }
