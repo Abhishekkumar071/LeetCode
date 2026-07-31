2a5b37d7d24f18636085774ae5b6a87d37e7c346
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-07-31 10:16:30 +0530

    Time: 11 ms (34.10%) | Memory: 46.2 MB (100.00%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+        for(char ch: word.toCharArray()){
+            freq[ch-'a']++;
+        }
+        Arrays.sort(freq);
+        int ans=0;
+        for(int i=25; i>=0; i--){
+            ans += ((25-i)/8+1)*freq[i];
+        }return ans;
+    }
+}
