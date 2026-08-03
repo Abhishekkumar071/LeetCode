3208f0d6d21b71346bdca2a65fe29838f4e74367
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-08-03 10:35:08 +0530

    Time: 12 ms (6.28%) | Memory: 46.6 MB (22.34%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,17 @@
 class Solution {
+    int[][] memo;
+    int maxDiff(int i, int j, int[] nums){
+        if(i==j) return nums[i];
+        if(memo[i][j]!=-1) return memo[i][j];
+        return  memo[i][j]=Math.max(
+            nums[i]-maxDiff(i+1, j, nums),
+            nums[j]-maxDiff(i, j-1, nums)
+        );
+    }
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n=piles.length;
+        memo = new int[n][n];
+        for(int[] mm: memo)Arrays.fill(mm, -1);
+        return maxDiff(0, n-1, piles)>0;
     }
 }
