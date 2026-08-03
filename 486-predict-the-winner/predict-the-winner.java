8ff5fb21910bac0f5ff8993158a10d3c32afa009
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-08-03 10:53:58 +0530

    Time: 1 ms (38.31%) | Memory: 42.8 MB (49.37%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -7,6 +7,26 @@ class Solution {
         );
     }
     public boolean predictTheWinner(int[] nums) {
-        return maxDiff(0, nums.length-1, nums)>=0;
+        // return maxDiff(0, nums.length-1, nums)>=0;
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+        
+                int j = i + len - 1;
+        
+                if (i == j)
+                    dp[i][j] = nums[i];
+                else
+                    dp[i][j] = Math.max(
+                        nums[i] - dp[i + 1][j],
+                        nums[j] - dp[i][j - 1]
+                    );
+            }
+        }
+        // for(int[] d: dp){
+        //     System.out.println(Arrays.toString(d));
+        // }
+        return dp[0][n-1]>=0;
     }
 }
