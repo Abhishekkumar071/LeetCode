b5727877798106a61c7422f1d5438bb6deaea3e4
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-08-03 22:43:25 +0530

    Time: 4 ms (98.01%) | Memory: 85.2 MB (83.24%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -1,26 +1,19 @@
 class Solution {
-    int n;
-    int[] memo;
-    int maxDiff(int i, int[] arr){
-        if(i==n) return 0;
-        if(memo[i]!=-1) return memo[i];
-        int a=Integer.MIN_VALUE;
-        int b=Integer.MIN_VALUE;
-        int c=Integer.MIN_VALUE;
-        a = arr[i]-maxDiff(i+1, arr);
-        if(i+2<=n){
-            b = arr[i]+arr[i+1]-maxDiff(i+2, arr);
-        }
-        if(i+3<=n){
-            c = arr[i]+arr[i+1]+arr[i+2]-maxDiff(i+3, arr);
-        }
-        return memo[i]= Math.max(a, Math.max(b, c));
-    }
     public String stoneGameIII(int[] ston) {
-        n=ston.length;
-        memo = new int[n];
-        Arrays.fill(memo, -1);
-        int diff = maxDiff(0, ston);
+        int n = ston.length;
+        int[] dp = new int[n+1];
+        dp[n] = 0; 
+        for(int i=n-1; i>=0; i--){
+            int a = ston[i]-dp[i+1];
+            int b=Integer.MIN_VALUE;
+            int c=Integer.MIN_VALUE;
+            if(i+2<=n){
+                b = ston[i]+ston[i+1]-dp[i+2];
+            }
+            if(i+3<=n) c=ston[i]+ston[i+1]+ston[i+2]-dp[i+3];
+            dp[i]=Math.max(a, Math.max(b, c));
+        }
+        int diff = dp[0];
         if(diff>0)return "Alice";
         else if(diff<0) return "Bob";
         else return "Tie";
