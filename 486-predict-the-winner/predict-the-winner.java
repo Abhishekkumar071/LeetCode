e17ca534f2cbb55418b73eac8178c7301682fd60
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-08-03 10:28:51 +0530

    Time: 65 ms (20.11%) | Memory: 42.5 MB (79.18%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,25 +1,12 @@
 class Solution {
+    int maxDiff(int i, int j, int[] nums){
+        if(i==j) return nums[i];
+        return Math.max(
+            nums[i]-maxDiff(i+1, j, nums),
+            nums[j]-maxDiff(i, j-1, nums)
+        );
+    }
     public boolean predictTheWinner(int[] nums) {
-        int n = nums.length;
-
-        int[][] dp = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = nums[i];
-        }
-
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1;
-
-                int takeLeft = nums[i] - dp[i + 1][j];
-
-                int takeRight = nums[j] - dp[i][j - 1];
-
-                dp[i][j] = Math.max(takeLeft, takeRight);
-            }
-        }
-
-        return dp[0][n - 1] >= 0;
+        return maxDiff(0, nums.length-1, nums)>=0;
     }
 }
